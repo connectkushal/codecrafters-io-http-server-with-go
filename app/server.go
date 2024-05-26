a8f276772574ea7fccae88893b15ba636d3e1c06
Kushal <connectkushal@gmail.com>
2024-05-26 19:12:06 +0530

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"flag"
 	"fmt"
+	"log"
 	"net"
 	"os"
 	"path"
@@ -43,13 +44,14 @@ func main() {
 	}
 }
 
-func handleConnections(c net.Conn) error {
+func handleConnections(c net.Conn) {
 	defer c.Close()
 
 	buffer := make([]byte, 1024)
 	_, err := c.Read(buffer)
 	if err != nil {
-		return err
+		log.Println(err)
+		return
 	}
 
 	req := ParseRequest(buffer)
@@ -58,19 +60,32 @@ func handleConnections(c net.Conn) error {
 
 	switch {
 	case req.Target == "/":
-		return handleResponse(c, "HTTP/1.1 200 OK\r\n\r\n")
+		handleResponse(c, "HTTP/1.1 200 OK\r\n\r\n")
+		return
 
 	case strings.HasPrefix(req.Target, "/echo/"):
+		v, ok := req.Headers["accept-encoding"]
+		fmt.Println(v)
 		msg := strings.Split(req.Target, "/")[2]
+
+		if ok {
+			if v == "gzip" {
+				response := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Encoding: gzip\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(msg), msg)
+				handleResponse(c, response)
+				return
+			}
+		}
+
 		//fmt.Println(msg)
 		var response = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(msg), msg)
 
-		return handleResponse(c, response)
-
+		handleResponse(c, response)
+		return
 	case req.Target == "/user-agent":
-		var response = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(req.Headers["User-Agent"]), req.Headers["User-Agent"])
+		var response = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(req.Headers["user-agent"]), req.Headers["user-agent"])
 
-		return handleResponse(c, response)
+		handleResponse(c, response)
+		return
 
 	case strings.HasPrefix(req.Target, "/files/"):
 		filename := strings.Split(req.Target, "/")[2]
@@ -82,27 +97,37 @@ func handleConnections(c net.Conn) error {
 			contents, err := os.ReadFile(path)
 			if err != nil {
 				fmt.Println("File NOT FOUND: \n\tfilename:", filename, "\n\tdirectory:", path)
+				log.Println(err)
 				c.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
-				return err
+				return
 			}
 
 			fmt.Println("file exists, reading file:", path)
 			response := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: %d\r\n\r\n%s", len(contents), string(contents))
-			return handleResponse(c, response)
+			handleResponse(c, response)
+			return
 
 		case "POST":
+			if len(filename) == 0 {
+				//TODO: Use the correct status for bad input
+				c.Write([]byte("HTTP/1.1 503 Internal Server Error\r\n\r\n Filename cannot be empty"))
+				return
+			}
+
 			err := os.WriteFile(path, []byte(req.Body), 0644)
 			if err != nil {
+				log.Println(err)
 				c.Write([]byte("HTTP/1.1 503 Internal Server Error\r\n"))
-				return err
+				return
 			}
-			return handleResponse(c, "HTTP/1.1 201 Created\r\n\r\n")
+			handleResponse(c, "HTTP/1.1 201 Created\r\n\r\n")
+			return
 		}
 
 	default:
-		return handleResponse(c, "HTTP/1.1 404 Not Found\r\n\r\n")
+		handleResponse(c, "HTTP/1.1 404 Not Found\r\n\r\n")
+		return
 	}
-	return nil
 }
 
 func ParseRequest(b []byte) Request {
@@ -125,15 +150,17 @@ func ParseRequest(b []byte) Request {
 
 	for _, v := range raw_headers {
 		h := strings.Split(v, ": ")
-		r.Headers[h[0]] = h[1]
+		r.Headers[strings.ToLower(h[0])] = h[1]
 	}
 
 	return r
 }
 
-func handleResponse(c net.Conn, s string) error {
+func handleResponse(c net.Conn, s string) {
 	_, err := c.Write([]byte(s))
-	return err
+	if err != nil {
+		log.Println(err)
+	}
 }
 
 // func notFoundResponse(c net.Conn) error {
